653674a5095ced35a665f985356e9c6e3628650f
ianoti <ianodhiambootieno@gmail.com>
2017-02-09 07:34:19 +0300

    (chore) split serialiser and utility functions
    1. create separate file for serialiser
    2. create separate file for utility functions

--- app/utils.py
+++ app/utils.py
@@ -1,20 +1,5 @@
-from flask_restful import fields
 import string
-
-from . import db
-
-itemformat = {"id": fields.Integer,
-              "name": fields.String,
-              "date_created": fields.DateTime(dt_format="rfc822"),
-              "date_modified": fields.DateTime(dt_format="rfc822"),
-              "done": fields.Boolean(attribute="status")}
-
-bucketlistformat = {"id": fields.Integer,
-                    "name": fields.String,
-                    "items": fields.List(fields.Nested(itemformat)),
-                    "date_created": fields.DateTime(dt_format="rfc822"),
-                    "date_modified": fields.DateTime(dt_format="rfc822"),
-                    "created_by": fields.String(attribute="user.username")}
+from app import db
 
 
 def validate_string(string_passed):
