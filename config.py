75120f2f19833189723bb0f48edbe7435b2891ef
ianoti <ianodhiambootieno@gmail.com>
2017-02-09 10:32:49 +0300

    (chore) added configuration setup file
    1. added Development environment setup
    2. added Testing environment setup

---
+++ config.py
@@ -0,0 +1,46 @@
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config:
+    """
+    This holds the default configuration settings that will be inherited
+    for the software environment setup
+    """
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'check_point_rules'
+    SQLALCHEMY_COMMIT_ON_TEARDOWN = True
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    @staticmethod
+    def init_app(app):
+        pass
+
+
+class Development(Config):
+    """
+    The development environment used throughout setup
+    """
+    DEBUG = True
+    # the following configuration defines postgres database for developmement
+    """
+    app.config['SQLALCHEMY_DATABASE_URI'] = ('postgresql://
+                                             bucketlist:bucketlist@localhost
+                                             /bucketlist')
+    """
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'data-dev.db')
+
+
+class Testing(Config):
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'data-test.db')
+
+configset = {
+    "development": Development,
+    "testing": Testing,
+    "default": Development
+    }
+
+expiry_time = 3600
